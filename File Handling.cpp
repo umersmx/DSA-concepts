2956f57826627a2b2b0071869e0bd8fb9b038e99
Umer <umerfarooqsamsung@gmail.com>
2026-01-26 15:26:44 +0500

    Add files via upload

---
+++ File Handling.cpp
@@ -0,0 +1,15 @@
+//FILE HANDLING
+#include <fstream>
+//keywords (used to open and access a file in code): 
+//ofstream [creation + reading of the file]
+//ifstream [reading only]
+//keywords (general):
+//noskipws [dont skip white spaces]
+//built in function:
+//eof(): End of file
+//syntax: make an object
+// ofstream obj("nameoffile.txt" :: ios.app) [creating a file]\
+//WHhenever we access a file, closing it is a must through: filename.close();
+//Database basic operations: CRUD (Create, Read, Update, Delete)
+#include<iostream>
+using namespace std;
